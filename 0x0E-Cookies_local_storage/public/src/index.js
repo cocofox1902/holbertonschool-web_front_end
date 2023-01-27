756b39588f69bd5424d51343a6280f26ee404f59
cocofox1902 <colas.renard1902@gmail.com>
2023-01-27 11:10:48 +0100

    add all

---
+++ 0x0E-Cookies_local_storage/public/src/index.js
@@ -0,0 +1,12 @@
+function setCookies() {
+  const firstname = document.getElementById("firstname").value;
+  const email = document.getElementById("email").value;
+  document.cookie = `firstname=${firstname}`;
+  document.cookie = `email=${email}`;
+}
+
+function showCookies() {
+  const p = document.createElement("p");
+  p.innerHTML = `Cookies: ${document.cookie}`;
+  document.body.appendChild(p);
+}
